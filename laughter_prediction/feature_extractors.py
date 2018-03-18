52555671c3fd33089d630363199e78455e059993
Nikitosh <npodguzov@yandex.ru>
2018-03-18 20:56:46 +0300

    Add second homework

---
+++ laughter_prediction/feature_extractors.py
@@ -0,0 +1,51 @@
+import os
+import tempfile
+import librosa
+import pandas as pd
+import numpy as np
+import scipy.io.wavfile as wav
+
+class FeatureExtractor:
+    def extract_features(self, wav_path):
+        """
+        Extracts features for classification ny frames for .wav file
+
+        :param wav_path: string, path to .wav file
+        :return: pandas.DataFrame with features of shape (n_chunks, n_features)
+        """
+        raise NotImplementedError("Should have implemented this")
+
+
+class PyAAExtractor(FeatureExtractor):
+    """Python Audio Analysis features extractor"""
+    def __init__(self):
+        self.extract_script = "./extract_pyAA_features.py"
+        self.py_env_name = "ipykernel_py2"
+
+    def extract_features(self, wav_path):
+        with tempfile.NamedTemporaryFile() as tmp_file:
+            feature_save_path = tmp_file.name
+            cmd = "python \"{}\" --wav_path=\"{}\" " \
+                  "--feature_save_path=\"{}\"".format(self.extract_script, wav_path, feature_save_path)
+            os.system("source activate {}; {}".format(self.py_env_name, cmd))
+
+            feature_df = pd.read_csv(feature_save_path)
+        return feature_df
+
+class LibrosaExtractor(FeatureExtractor):
+    def __init__(self, frame_sec=0.5):
+        self.frame_sec = frame_sec
+
+    def extract_features(self, wav_path):
+        rate, audio = wav.read(wav_path)
+        audio = audio.astype(np.float64)
+        length = audio.shape[0]
+        frame_length = int(rate * self.frame_sec)
+        frame_shift = frame_length // 4
+        features = []
+        for i in range(0, length - frame_length + 1, frame_shift):
+            features.append(np.concatenate((
+                np.mean(librosa.feature.mfcc(audio[i: i + frame_length], rate).T, axis=0),
+                np.mean(librosa.feature.melspectrogram(audio[i: i + frame_length], rate).T, axis=0)
+            )))
+        return pd.DataFrame(np.array(features))
---
+++ laughter_prediction/process_audio.py
@@ -0,0 +1,92 @@
+import argparse
+import json
+import numpy as np
+
+
+def intervals_gen(timestamps, frame_sec, error_dist=None, min_frames=None):
+    """
+    Generator returning target class intervals from timestamps
+
+    :param timestamps: array-like, starting point of each frame in sec
+    :param frame_sec: int, length of each timeframe in seconds
+    :param error_dist: error_dist: int, allowed length of misclassified audio frame in sec
+    :param min_frames: int, minimal valid interval length in sec
+    :return: generator returning pairs: (interval_start, interval_end)
+    """
+    if min_frames is None:
+        min_frames = 10
+    if error_dist is None:
+        error_dist = 1.5 * frame_sec
+
+    begin = 0
+    length = len(timestamps)
+    if length <= 1:
+        return
+    for i in range(1, length):
+        if timestamps[i] - timestamps[i - 1] >= error_dist:
+            if i - 1 - begin > min_frames:
+                yield timestamps[begin], timestamps[i - 1]
+            begin = i
+    if begin != length - 1 and length - 1 - begin > min_frames:
+        yield timestamps[begin], timestamps[length - 1]
+
+
+def predicted_to_intervals(pred_classes, frame_sec, frame_shift):
+    start = -1
+    last = -1
+    current_time = 0
+    intervals = []
+    for pred_class in pred_classes:
+        if pred_class:
+            if current_time > last:
+                if start != last:
+                    intervals.append((start, last))
+                start = current_time
+            last = current_time + frame_sec
+        current_time += frame_shift
+    if start != last:
+        intervals.append((start, last))
+    return intervals
+
+def my_import(name):
+    """
+    Imports class for any module by name
+
+    :param name: string, dot-separated full class name
+    :return: callable, returning class instance
+    """
+
+    # http://stackoverflow.com/questions/547829/how-to-dynamically-load-a-python-class
+    components = name.split('.')
+    mod = __import__(components[0])
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+
+def main():
+    DEFAULT_PARAMS = "params/default_params.json"
+
+    parser = argparse.ArgumentParser(description='Script for prediction laughter intervals for .wav file')
+    parser.add_argument('--wav_path', type=str, help='Path to .wav file')
+    parser.add_argument('--params', type=str, default=DEFAULT_PARAMS,
+                        help='/JSON file with the classification parameters. Default: ' + DEFAULT_PARAMS + '.')
+    args = parser.parse_args()
+
+    with open(args.params, 'r') as params_file:
+        params = json.load(params_file)
+
+    klass = my_import(params['predictor'])
+    predictor = klass()
+    klass = my_import(params['extractor'])
+    extractor = klass()
+
+    feature_df = extractor.extract_features(args.wav_path)
+    pred_classes = predictor.predict(np.expand_dims(feature_df, axis=0))[0]
+    intervals = predicted_to_intervals(pred_classes, frame_sec=0.5, frame_shift=0.5/4)
+    print("Target intervals")
+    print(intervals)
+
+
+if __name__ == '__main__':
+    main()
